0390ae012980e940eade3467486fd186675b97d9
Rick Roßberg <rossberg@theobald.local>
2017-05-24 14:40:09 +0200

    gitignore update Models package and first models added

---
+++ src/main/java/de/bankprogramming/models/Account.java
@@ -0,0 +1,18 @@
+/**
+* @ Author: Rick
+* @ Creation Date: 24.05.2017
+*/
+package de.bankprogramming.models;
+
+import java.util.Date;
+
+public class Account {
+
+	long customerID;
+	Date startDate;
+	Date endDate;
+
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/de/bankprogramming/models/Customer.java
@@ -0,0 +1,22 @@
+/**
+* @ Author: Rick
+* @ Creation Date: 24.05.2017
+*/
+package de.bankprogramming.models;
+
+import java.util.Date;
+
+public class Customer {
+
+	// fields
+	long customerId;
+	String name;
+	String address;
+	String telNumber;
+	Date dateOfBirth;
+	boolean isMale;
+
+	public Customer() {
+
+	}
+}
