5fb1c4720cb5c6a43aca0b382bb1393c5f27484c
Dorian Czichotzki <dorian.czichotzki@de.ibm.com>
2017-05-26 16:39:12 +0200

    Removed Java 1.5 Added Main

--- src/main/java/de/bankprogramming/Main.java
+++ src/main/java/de/bankprogramming/Main.java
@@ -1,7 +1,18 @@
 package de.bankprogramming;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by puruckel on 23.05.2017.
  */
-public class Main {
+public class Main extends Application{
+
+    public void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
